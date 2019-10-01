3f25af4fe4b5ffa5f5bd83ed4efa19fdcff4016b
Timm Schoenborn <timmschoenborn@gmail.com>
2019-10-01 15:30:16 -0500

    setup placeholder Tomagotchi, took notes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-console.log("js works");
+// console.log("js works");
+
+// class Tomagotchi{}
+	//things the tomagotchi can do are methods of the tomagotchi
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// below this line is the game object, describes what events do when the user interacts with them
+
+// const game{}
+	//properties outside of the tomagotchi, like timer
+
+////DO NOT HAVE MORE THAN ONE INTERVAL////
+
+	//objects to feed the tomagotchi
+	//
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////listeners can go in global scope but don't necessarily need to be
+		// no logic down here
+		// all that exists here are the events that call the methods from the tomagotchi and game objects
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
